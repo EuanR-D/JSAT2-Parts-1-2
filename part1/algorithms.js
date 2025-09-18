f1ee3d76633ac611aef476c3225c44b5d6651ff9
EuanR-D <erenfreydouglas@gmail.com>
2025-09-18 14:15:26 +1000

    changed comment
    Just rewrote the comment cuz i didnt like the old one

--- part1/algorithms.js
+++ part1/algorithms.js
@@ -5,7 +5,8 @@ console.log(myArr);
 
 // Q1.2
 // Array of numbers sorted from smallest to largest (ascending)
-// toSorted() compares 2 numbers and rearranges based on 1st symbol of value and creates new array
-// function(a, b) compares the whole value and brings 'a' forward if -neg and back if +pos
+// toSorted() sorts the array based on the initial of each primitive
+// function(a, b) changes toSorted() to sort by whole value
 let myArrAsc = myArr.toSorted(function(a, b){return a - b});
 console.log(myArrAsc);
+
