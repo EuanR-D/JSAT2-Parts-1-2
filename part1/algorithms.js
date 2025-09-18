cbe35c8ea015666d324572c6992084075311b637
EuanR-D <erenfreydouglas@gmail.com>
2025-09-18 14:53:45 +1000

    Q1.5 SeqSearch
    Added sequential search function

--- part1/algorithms.js
+++ part1/algorithms.js
@@ -22,3 +22,27 @@ console.log(myArrAsc);
 myArrAsc.splice(myArrAsc.indexOf(8), 1);
 myArrAsc.splice(myArrAsc.indexOf(31), 1);
 console.log(myArrAsc);
+
+
+// Q1.5
+// function to search through array from start
+function sequentialSearch(arr, target) {
+  // result variable set to default value for not found
+  let foundIndex = -1;
+
+  // for loop to iterate through each item off array
+  for(let i = 0; i <= arr.length; i++) {
+    // if statement to compare current item to target value
+    if (arr[i] == target) {
+      // updates result to position of target value and stops
+      foundIndex = i;
+      break;
+    }
+  }
+  // returning value of result
+  return foundIndex;
+}
+
+console.log(sequentialSearch(myArrAsc, 30));
+
+console.log(sequentialSearch(myArrAsc, 12));
